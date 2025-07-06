401035787936a502698154409302680c611a8136
youssefhossam29 <yh5719731@gmail.com>
2025-07-06 18:50:04 +0300

    add custom validator to prevent email duplication across users and admins

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -19,5 +20,18 @@ class ProfileUpdateRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $email = $this->input('email');
+
+            $adminWithSameEmail = Admin::where('email', $email)->first();
+
+            if ($adminWithSameEmail) {
+                $validator->errors()->add('email', 'The email has already been taken.');
+            }
+        });
     }
 }
