ed821bbd04cd7c815de1a62d0ec7b0c2937da383
youssefhossam29 <yh5719731@gmail.com>
2025-07-06 22:42:54 +0300

    Fix namespace for AdminLoginRequest in AuthenticatedSessionController

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\Admin\AdminLoginRequest;
+use App\Http\Requests\Admin\Auth\AdminLoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
